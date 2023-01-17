b2aa312984398c63ec8f49f22c3fa140b78765aa
TeiHek <lerandy1235@gmail.com>
2023-01-17 11:15:05 -0800

    Added TMP, basic UI textbox layout

---
+++ Assets/Randy/DialogueManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueManager : MonoBehaviour
+{
+    [SerializeField] private float textRenderDelay;
+    [SerializeField] private float nextLineDelay;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
