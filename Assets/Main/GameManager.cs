9849d7594138cb1cf452af7d8bc1996dbc35189b
LucianRain <aidanbacon@gmail.com>
2023-01-19 22:53:53 -0800

    added GameManager

---
+++ Assets/Main/GameManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    void Awake () { instance = this; }
+
+    GameObject player;
+
+    public void SetPlayer(GameObject _player)
+    {
+        player = _player;
+    }
+
+    public GameObject GetPlayer()
+    {
+        return player;
+    }
+}
