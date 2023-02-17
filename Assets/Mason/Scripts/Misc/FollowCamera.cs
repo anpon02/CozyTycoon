03d594e3e177330af8d0223f7e9dd4a76049d2bc
Koobysnack <97823937+Koobysnack@users.noreply.github.com>
2023-02-17 12:18:16 -0800

    using tag shenanigans

--- Assets/Mason/Scripts/Misc/FollowCamera.cs
+++ Assets/Mason/Scripts/Misc/FollowCamera.cs
@@ -1,8 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using Cinemachine;
-using UnityEngine.InputSystem.Android;
 
 public class FollowCamera : MonoBehaviour
 {
