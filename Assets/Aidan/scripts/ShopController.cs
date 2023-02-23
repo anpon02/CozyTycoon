81464a4363cc0027b3a237b2ed42d712ba8a8119
LucianRain <aidanbacon@gmail.com>
2023-02-23 08:43:28 -0800

    wip item finalizing, reenabled shop

--- Assets/Aidan/scripts/ShopController.cs
+++ Assets/Aidan/scripts/ShopController.cs
@@ -9,16 +9,17 @@ public class ShopController : MonoBehaviour
     [SerializeField] ShopCoordinator coord;
     [SerializeField] GameObject content, shopButton;
     [SerializeField] int openshopSound;
+    [SerializeField] bool startEnabled;
 
     private void Start()
     {
         AudioManager.instance.PlaySound(openshopSound, gameObject);
-        shopButton.SetActive(false);
+        shopButton.SetActive(startEnabled);
     }
 
     public void ToggleVisible()
     {
-        content.SetActive(!gameObject.activeInHierarchy);
+        content.SetActive(!content.activeInHierarchy);
         if (content.activeInHierarchy) DisplayProducts();
         shopButton.SetActive(true);
     }
